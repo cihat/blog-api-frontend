39ef09990320725d32734b970576fa7ddab17e1b
cihat <cihat02020202@gmail.com>
2021-11-24 21:47:54 +0300

    Add fetchPosts and login action, mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,38 @@
-import { createStore } from "vuex";
+import { createStore } from "vuex"
+import axios from "axios"
+
+const axiosInstance = axios.create({
+  baseURL: "https://blog-api-cihat.herokuapp.com/",
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    posts: [],
+    username: "test",
+    password: "test",
+    user: {},
+  },
+  mutations: {
+    setPosts(state, payload) {
+      state.posts = payload
+    },
+    setPost(state, payload) {
+      state.post = payload
+    },
+  },
+  actions: {
+    async fetchPosts({ commit }) {
+      const res = await axiosInstance.get("/posts")
+      commit("setPosts", res.data)
+    },
+
+    async login({ commit, state }) {
+      await axiosInstance.post("/login", { username: state.username, password: state.password })
+    }
+  },
   modules: {},
-});
+})
